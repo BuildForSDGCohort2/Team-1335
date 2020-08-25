e1001930cdd7e514b6762efa01e74171dbb59c11
tamalesimon <tamalesp@gmail.com>
2020-08-26 01:02:57 +0300

    homepage title setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,8 @@ import React from 'react';
 function App() {
   return (
     <div className='container mx-auto'>
-      <header>
-        <h1>Traffic Offense and Fine Tracker</h1>
+      <header className="flex justify-center mt-3">
+        <h1 className='font-bold text-xl text-blue-600'>Traffic Offense and Fine Tracker</h1>
       </header>
     </div>
   );
